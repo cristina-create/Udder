1b5fe1f8a577ab6725ec2b77bd790a49ddb08d5f
Cristina Danc <cristina.danc@gmail.com>
2020-08-04 14:56:17 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Cristina Danc on 04/08/2020.
+//  Copyright © 2020 Cristina Danc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
